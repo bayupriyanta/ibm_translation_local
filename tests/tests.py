1e82a70d92cc579345040b24cbf0dc5e30951652
BayuPriyanta <bayu.priyanta1@gmail.com>
2022-10-21 12:09:09 +0700

    package structure
    package structure

---
+++ tests/tests.py
@@ -0,0 +1,8 @@
+from translator import english_to_french
+from translator import french_to_english
+
+translate1 = english_to_french('Hello')
+print("Hello in french is "+translate1)
+
+translate2 = french_to_english('Bonjour')
+print("Bounjour means "+translate2)
