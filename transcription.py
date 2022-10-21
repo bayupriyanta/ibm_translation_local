5b02f4ad72604463debdb4f2907d58120dd9e41b
BayuPriyanta <bayu.priyanta1@gmail.com>
2022-10-21 11:41:42 +0700

    initial
    initial code

---
+++ transcription.py
@@ -0,0 +1,23 @@
+import json
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+apikey = os.environ['apikey']
+url = os.environ['url']
+authenticator = IAMAuthenticator(apikey)
+language_translator = LanguageTranslatorV3(version='2022-10-21',authenticator=authenticator)
+language_translator.set_service_url(url)
+
+def englishToFrench(englishText):
+        frenchTranslate = language_translator.translate(text=text1),model_id='en-fr').get_result()
+        frenchText = frenchTranslate.get("translations")[0].get("translation")
+        return frenchText
+
+def frenchToEnglish(frenchText):
+    englishTranslate = language_translator.translate(text=text1),model_id='fr-en').get_result()
+    englishText = englishTranslate.get("translations")[0].get("translation")
+    return englishText
